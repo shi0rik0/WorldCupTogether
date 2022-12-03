d64ee94c55a679d6d20b8858abedeb481f692e6d
J <ogas_dandelion@sjtu.edu.cn>
2022-12-03 19:42:06 +0800

    加了一个空的activity

---
+++ app/src/main/java/com/ss/video/rtc/demo/quickstart/VedioSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.ss.video.rtc.demo.quickstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VedioSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vedio_selection);
+    }
+}
